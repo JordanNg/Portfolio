9fc98334d5b0e5c34e47ab3af85ab23d038e1c05
Jordan Ng <ngjor@oregonstate.edu>
2020-08-14 15:51:45 -1000

    Refactored projects and gave cards new css.

---
+++ src/projectArray.js
@@ -0,0 +1,74 @@
+const projectArray = [
+  {
+    projectTitle: "Food Crate",
+    description:
+      "An Android mobile application that allows indecisive foodies to quickly decide on their next meal.",
+    techArray: [
+      "https://2.bp.blogspot.com/-tzm1twY_ENM/XlCRuI0ZkRI/AAAAAAAAOso/BmNOUANXWxwc5vwslNw3WpjrDlgs9PuwQCLcBGAsYHQ/s1600/pasted%2Bimage%2B0.png",
+      "https://www.mazewomenshealth.com/wp-content/uploads/2019/07/Yelp_trademark_RGB2.png",
+    ],
+  },
+  {
+    projectTitle: "Othello AI",
+    description:
+      "AI that uses the MINIMAX algorithm in a simplified game of Othello. AI attempts to play optimally against a human player.",
+    techArray: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
+    ],
+  },
+  {
+    projectTitle: "Netflix Clone",
+    description:
+      "A clone of Netflix's web application. Implemented using React and hosted with Firebase. Movies are pulled via The Movie Database's API.",
+    techArray: [
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+      "https://4.bp.blogspot.com/-rtNRVM3aIvI/XJX_U07Z-II/AAAAAAAAJXY/YpdOo490FTgdKOxM4qDG-2-EzcNFAWkKACK4BGAYYCw/s1600/logo%2Bfirebase%2Bicon.png",
+    ],
+  },
+  {
+    projectTitle: "Portfolio",
+    description:
+      "This portfolio's code! I used this portfolio as a way to learn React, Firebase, and other technologies. It includes a small grid of Conway's Game of Life which starts with a random board each time.",
+    techArray: [
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+      "https://4.bp.blogspot.com/-rtNRVM3aIvI/XJX_U07Z-II/AAAAAAAAJXY/YpdOo490FTgdKOxM4qDG-2-EzcNFAWkKACK4BGAYYCw/s1600/logo%2Bfirebase%2Bicon.png",
+    ],
+  },
+  {
+    projectTitle: "Instagram Clone",
+    description:
+      "A React implementation of a simplified Instagram web application. This clone uses Firebase as a backend for storing pictures and comments.",
+    techArray: [
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+      "https://4.bp.blogspot.com/-rtNRVM3aIvI/XJX_U07Z-II/AAAAAAAAJXY/YpdOo490FTgdKOxM4qDG-2-EzcNFAWkKACK4BGAYYCw/s1600/logo%2Bfirebase%2Bicon.png",
+      "https://material-ui.com/static/logo.png",
+    ],
+  },
+  {
+    projectTitle: "Yelp Review Sentiment",
+    description:
+      "AI that uses a training set to determine the sentiment of Yelp Reviews. Implemented with the Naive Bayes Bayesian Network",
+    techArray: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
+    ],
+  },
+  {
+    projectTitle: "Wolves and Chickens",
+    description:
+      "The classic wolves and chickens problem solved using Breadth First Search, Depth First Search, Iterative Deepening Depth First Search, and A Star Search.",
+    techArray: [
+      "https://cdn.iconscout.com/icon/free/png-512/c-programming-569564.png",
+    ],
+  },
+  {
+    projectTitle: "Wireless Heatmap",
+    description:
+      "A heat-map of Oregon State University's wireless network. Implemented using Django and the Google Maps Javascript API.",
+    techArray: [
+      "https://cdn.worldvectorlogo.com/logos/django.svg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Google_Maps_icon_%282020%29.svg/1200px-Google_Maps_icon_%282020%29.svg.png",
+    ],
+  },
+];
+
+export default projectArray;
